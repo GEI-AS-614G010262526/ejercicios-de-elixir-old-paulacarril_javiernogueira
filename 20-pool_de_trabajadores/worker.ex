c8ab6a298a2fdf28cef6f60a386fbd4b036e713d
paulacarril <145045483+paulacarril@users.noreply.github.com>
2025-09-30 10:10:37 +0200

    feat(worker): implement worker process with loop and error handling

--- 20-pool_de_trabajadores/worker.ex
+++ 20-pool_de_trabajadores/worker.ex
@@ -3,5 +3,27 @@ defmodule Trabajador do
     Trabajdores para el _Servidor_
   """
 
+  @spec start() :: pid()
+  def start() do
+    spawn(fn -> loop() end)
+  end
 
+  defp loop() do
+    receive do
+      {:trabajo, from, func} ->
+        resultado = work(func)
+        send(from, {:resultado, self(), resultado})
+        loop()
+
+      :stop -> :ok
+    end
+  end
+
+  defp work(func) do
+    try do
+      func.()
+    rescue
+      error -> {:error, error}
+    end
+  end
 end
