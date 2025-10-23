f40ccccb6e3bf8fafdcb8eab076c5e4acc959b30
vicho1822 <javitonogueira@gmail.com>
2025-10-23 19:51:37 +0200

    add: actor module

---
+++ 50-servidores-federados/federal_server/lib/federated/actor.ex
@@ -0,0 +1,14 @@
+def module Federated.Actor do
+  @moduledoc"""
+
+  """
+
+  defstruct {:id, :name, :avatar, inbox: []}
+
+  def new(id, name, avatar) do
+    %__MODULE__{id: id, name: name, avatar: avatar, inbox: []}
+  end
+
+  def server_name(%__MODULE__{id: id}), do: String.split(id, "@") |> Enum.at(1)
+  def username(%__MODULE__{id: id}), do: String.split(id, "@") |> Enum.at(0)
+end
